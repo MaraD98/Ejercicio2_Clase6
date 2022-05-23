5d0a14716b101981c2b42812a45aea88ff9a44ab
MaraD98 <maradelgadillo2020@gmail.com>
2022-05-23 10:41:38 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+int contador = 0;
+int numeroIngresado;
+
+do
+{
+    
+    Console.WriteLine("Ingrese un número entre en el 1 y el 20 por favor: ");
+    numeroIngresado = int.Parse(Console.ReadLine());
+
+    if (numeroIngresado != numeroSecreto)
+    {
+        if (numeroIngresado > numeroSecreto)
+        {
+            Console.WriteLine($"El numero {numeroIngresado} es muy GRANDE..!\nVuelva a intentarlo...");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numeroIngresado} es muy CHICO...!\nVuelva a intentarlo...");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"¡¡Felicitaciones!! Has adivinado, el número secreto era: **{numeroSecreto}**\n Lo has logrado en {contador+1} intentos!!");
+        break;
+    }
+
+    contador++;
+} while (true);
+
